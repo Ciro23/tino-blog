ff4524ba25e5ffa2016b14373699e183a00fcb16
ciro23 <ciro.sorre23@gmail.com>
2024-03-06 23:00:10 +0100

    add spring security

--- src/main/java/it/tino/blog/application/BlogApplication.java
+++ src/main/java/it/tino/blog/application/BlogApplication.java
@@ -1,4 +1,4 @@
-package it.tino.blog;
+package it.tino.blog.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/it/tino/blog/application/ServletInitializer.java
+++ src/main/java/it/tino/blog/application/ServletInitializer.java
@@ -1,4 +1,4 @@
-package it.tino.blog;
+package it.tino.blog.application;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -9,5 +9,4 @@ public class ServletInitializer extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(BlogApplication.class);
     }
-
 }
