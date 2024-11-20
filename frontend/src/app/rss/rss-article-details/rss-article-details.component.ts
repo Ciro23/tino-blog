a97b4bcad48bae455c21c6508fddb25b739f7d5e
ciro23 <ciro.sorre23@gmail.com>
2024-11-20 14:51:31 +0100

    fix rss article iframe and codeblocks
    iframe were allowed to be displayed by disabling the html sanitization.
    code blocks' style are now almost identical to the one used by
    ngx-markdown (except for syntax highlighting)

--- frontend/src/app/rss/rss-article-details/rss-article-details.component.ts
+++ frontend/src/app/rss/rss-article-details/rss-article-details.component.ts
@@ -1,8 +1,9 @@
-import {AfterViewInit, Component, Input, ViewEncapsulation} from '@angular/core';
+import {AfterViewInit, Component, Input, OnChanges, SimpleChanges, ViewEncapsulation} from '@angular/core';
 import {MarkdownComponent} from "ngx-markdown";
 import {NgIf} from "@angular/common";
 import {getFormattedCreationDateTime} from "../../utilities/date-utilities";
 import {Article} from "../../article/article";
+import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-rss-article-details',
@@ -14,8 +15,11 @@ import {Article} from "../../article/article";
   templateUrl: './rss-article-details.component.html',
   styleUrl: './rss-article-details.component.css'
 })
-export class RssArticleDetailsComponent implements AfterViewInit {
+export class RssArticleDetailsComponent implements AfterViewInit, OnChanges {
   @Input() article?: Article;
+  articleContent: SafeHtml = "";
+
+  constructor(private domSanitizer: DomSanitizer) {}
 
   ngAfterViewInit(): void {
     const figures = document.querySelectorAll('.content figure');
@@ -28,6 +32,24 @@ export class RssArticleDetailsComponent implements AfterViewInit {
       (img as HTMLImageElement).style.maxWidth = '100%';
       (img as HTMLImageElement).style.height = 'auto';
     });
+
+    // Syntax highlighting is not available for the RSS articles, but at least
+    // this makes the code blocks a bit more readable.
+    const codeBlocks = document.querySelectorAll('.content pre');
+    codeBlocks.forEach((codeBlock) => {
+      (codeBlock as HTMLPreElement).style.background = '#272822';
+      (codeBlock as HTMLPreElement).style.padding = '1rem';
+      (codeBlock as HTMLPreElement).style.borderRadius = '0.3rem';
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.article) {
+      // Sanitization must be bypassed because otherwise <iframe>, for example,
+      // would not be displayed. This opens the doors for XSS vulnerabilities,
+      // but I assume the RSS feeds are trusted.
+      this.articleContent = this.domSanitizer.bypassSecurityTrustHtml(this.article.content);
+    }
   }
 
   protected readonly getFormattedCreationDateTime = getFormattedCreationDateTime;
