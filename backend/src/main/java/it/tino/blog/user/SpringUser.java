d36ea3c43090007854bce4c2925fd8b74181f3d8
ciro23 <ciro.sorre23@gmail.com>
2024-11-22 17:14:12 +0100

    refactoring springuser

--- backend/src/main/java/it/tino/blog/user/SpringUser.java
+++ backend/src/main/java/it/tino/blog/user/SpringUser.java
@@ -43,24 +43,4 @@ public class SpringUser implements UserDetails {
     public String getUsername() {
         return email;
     }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
