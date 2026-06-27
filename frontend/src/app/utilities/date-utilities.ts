2f1aaba043922bbc5055a6c21099c1a52507f4a5
Ciro Sorrentino <ciro.sorrentino@pm.me>
2026-06-27 05:23:41 +0200

    add small comment

--- frontend/src/app/utilities/date-utilities.ts
+++ frontend/src/app/utilities/date-utilities.ts
@@ -1,3 +1,8 @@
+/**
+ * E.g. "Jun 27, 2026".
+ * This function also converts the UTC dates, received by the APIs, to local
+ * time.
+ */
 export function getFormattedCreationDateTime(date: Date): string {
   date = new Date(date);
   return new Intl.DateTimeFormat('en-US', {
